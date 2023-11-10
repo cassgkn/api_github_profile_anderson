37b168535f85c39130342e3d7e030110dba668a6
Anderson Casimiro <97445047+cassgkn@users.noreply.github.com>
2023-11-10 15:03:24 -0300

    Update main.js

--- main.js
+++ main.js
@@ -26,7 +26,7 @@ document.addEventListener('DOMContentLoaded', function() {
             }
         })
         try {
-            alert("Seja bem vindo ao perfil do GitHub o Anderson Casimiro! Cliqie em Ver no Github e saiba mais...");
+            alert("Seja bem vindo ao perfil do GitHub o Anderson Casimiro! Clique em Ver no Github e saiba mais...");
         }
         catch(err) {
             alert("Ocorreu um erro ao encontrar o perfil do Github");
